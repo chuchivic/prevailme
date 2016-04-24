16b9df6d354966cff0cbb9abed644bcf4153552b
Jesus <jesus.suarez@tecnilogica.com>
2016-04-24 20:53:24 +0200

    fixed untracked files

--- node_modules/botkit/lib/Botkit.js
+++
@@ -1,9 +0,0 @@
-var CoreBot = require(__dirname + '/CoreBot.js');
-var Slackbot = require(__dirname + '/SlackBot.js');
-var Facebookbot = require(__dirname + '/Facebook.js');
-
-module.exports = {
-    core: CoreBot,
-    slackbot: Slackbot,
-    facebookbot: Facebookbot,
-};
